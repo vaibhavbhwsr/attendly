d4fd522bd9bd19ad42b92f6645ae3a5d01245cbe
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2023-10-21 13:21:19 +0530

    redirect admin

--- attendly/attendly/urls.py
+++ attendly/attendly/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url='admin/', permanent=True)),
     path("admin/", admin.site.urls),
 ]
 
