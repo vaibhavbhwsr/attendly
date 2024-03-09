03ad9eb5320b726356c473bdc21293a3cc8d75e5
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2024-03-09 19:13:17 +0530

    fixtureg

--- attendly/entrypoint.sh
+++ attendly/entrypoint.sh
@@ -9,6 +9,7 @@ echo "PostgreSQL started ..."
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py loaddata fixtures/init.json
 # python manage.py runserver 0:8000
 
 exec "$@"
